dfa81db736701a733ad7c2acd332daccb13db385
WENCHO22 <wencho22@gmail.com>
2021-04-21 16:03:37 +0200

    implemented mentors feedback

--- databases/week2/homework-part-4.sql
+++ databases/week2/homework-part-4.sql
@@ -5,12 +5,12 @@ USE library_inventory;
 
 CREATE table `author`(
 `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-`name` varchar(255)
+`name` varchar(255) NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE table `genre`(
 `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-`name` varchar(255)
+`name` varchar(255) NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `publisher`(
@@ -24,21 +24,21 @@ CREATE table `book`(
 `description` text NOT NULL,
 `publisher_id` INT UNSIGNED NOT NULL,
 `rating` INT UNSIGNED NOT NULL,
-CONSTRAINT `fk_book_publisher` FOREIGN KEY (`publisher_id`) REFERENCES `publisher`(`id`) ON UPDATE CASCADE
+CONSTRAINT `fk_book_publisher` FOREIGN KEY (`publisher_id`) REFERENCES `publisher`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `book_genre`(
 `book_id` INT UNSIGNED NOT NULL,
 `genre_id` INT UNSIGNED NOT NULL,
 PRIMARY KEY(`book_id`, `genre_id`),
-CONSTRAINT `fk_book_genre_book` FOREIGN KEY(`book_id`) REFERENCES `book`(`id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_book_genre_genre` FOREIGN KEY(`genre_id`) REFERENCES `genre`(`id`) ON UPDATE CASCADE
+CONSTRAINT `fk_book_genre_book` FOREIGN KEY(`book_id`) REFERENCES `book`(`id`) ON DELETE CASCADE,
+CONSTRAINT `fk_book_genre_genre` FOREIGN KEY(`genre_id`) REFERENCES `genre`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `book_author`(
 `book_id` INT UNSIGNED NOT NULL,
 `author_id` INT UNSIGNED NOT NULL,
 PRIMARY KEY(`book_id`, `author_id`),
-CONSTRAINT `fk_book_author_book` FOREIGN KEY(`book_id`) REFERENCES `book`(`id`) ON UPDATE CASCADE,
-CONSTRAINT `fk_book_author_author` FOREIGN KEY(`author_id`) REFERENCES `author`(`id`) ON UPDATE CASCADE
+CONSTRAINT `fk_book_author_book` FOREIGN KEY(`book_id`) REFERENCES `book`(`id`) ON DELETE CASCADE,
+CONSTRAINT `fk_book_author_author` FOREIGN KEY(`author_id`) REFERENCES `author`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
