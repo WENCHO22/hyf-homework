a3eb24016f272ffabbf4becde194f7e5b0236eae
WENCHO22 <wencho22@gmail.com>
2021-05-21 16:35:25 +0200

    nodejs/week3 corrected

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -1,77 +1,49 @@
 const express = require("express");
 const app = express();
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", async(req, res) => res.send("nodejs week3 homework"));
+app.get("/", async (req, res) => res.send("nodejs week3 homework"));
 
 
-app.get("/add", (req, res) =>{
-    let values = Object.values(req.query)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let sum = values.reduce((a,b) => a + b)
-    res.send(`${sum}`)
+app.get("/calculator/:operation", (req, res) => {
+    const operation = req.params.operation
+    let values = Object.values(req.query).flat().map(n => Number(n));
+    if (values.every(item => typeof item === "number")) {
+        let result = 0;
+        if (operation === "add") {
+            result = values.reduce((a, b) => a + b)
+        } else if (operation === "subtract") {
+            result = values.reduce((a, b) => a - b)
+        } else if (operation === "multiply") {
+           result = values.reduce((a, b) => a * b)
+        } else if (operation === "division") {
+            result = values.reduce((a, b) => a / b)
+        }
+        res.send(`${result}`)
+    } else {
+        res.status(400).send({ error: "All values must be numbers" })
+    }
 });
 
 
-app.get("/substract", (req, res) =>{
-    let values = Object.values(req.query)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-
-    let subst = values.reduce((a,b) => a - b)
-    res.send(`${subst}`)
+app.post("/calculator/:operation", async (req, res) => {
+    const operation = req.params.operation
+    let values = Object.values(req.body).flat().map(n => Number(n));
+    console.log(values)
+    if (values.every(item => typeof item === "number")) {
+        if(operation === "add"){
+        var result = values.reduce((a, b) => a + b)
+        } else if(operation ==="subtract"){
+         var result = values.reduce((a, b) => a - b)   
+        } else if(operation === "multiply"){
+            var result = values.reduce((a, b) => a * b)
+        } else  if( operation === "division"){
+            var result = values.reduce((a, b) => a / b)
+        }
+        res.send(`${result}`)
+    } else {
+        res.status(400).send({ error: "All values must be numbers" })
+    }
 });
-
-app.get("/multiply", (req, res) =>{
-    let values = Object.values(req.query)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let mult = values.reduce((a,b) => a * b)
-    res.send(`${mult}`)
-});
-
-app.get("/division", (req, res) =>{
-    let values = Object.values(req.query)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let div = values.reduce((a,b) => a / b)
-    res.send(`${div}`)
-});
-
-
-/*----------------------------------------------------------------*/
-app.post("/add", async (req, res) =>{
-    let values = Object.values(req.body)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let sum = values.reduce((a,b) => a + b)
-    res.send(`${sum}`)
-})
-
-app.post("/substract", (req, res) =>{
-    let values = Object.values(req.body)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let subst = values.reduce((a,b) => a - b)
-    res.send(`${subst}`)
-});
-
-app.post("/multiply", (req, res) =>{
-    let values = Object.values(req.body)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let mult = values.reduce((a,b) => a * b)
-    res.send(`${mult}`)
-});
-
-app.post("/division", (req, res) =>{
-    let values = Object.values(req.body)
-    values = values.flat()
-    values = values.map(n=> Number(n))
-    let div = values.reduce((a,b) => a / b)
-    res.send(`${div}`)
-});
-
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
