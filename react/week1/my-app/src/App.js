392fd3106c0a01555dd63f1d76114c11879f7370
WENCHO22 <wencho22@gmail.com>
2021-05-25 00:29:44 +0200

    react/week1: implemented mentors feedback

--- react/week1/my-app/src/App.js
+++ react/week1/my-app/src/App.js
@@ -1,36 +1,10 @@
 //import logo from './logo.svg';
 import './App.css';
-import Header from "./Header.js";
-import ListItems from "./List.js";
-
-const toDoArray =[
-  {
-  description : "Clean the house",
-  dueDate: new Date(2020, 12, 25)
-},
-{
-  description : "Buy a new shirt",
-  dueDate: new Date(2021, 10, 20)
-},
-{description : "Finish my homework",
-  dueDate: new Date(2021, 5, 10)
-}
-]
-
-
-function HeaderWithList(){
-  return(
-    <div>
-    <Header title="To do list:" />
-    <ListItems list={toDoArray} />
-    </div>
-  )
-}
-
+import ToDoList from "./List.js";
 
 function App() {
   return (
-    <HeaderWithList />
+    <ToDoList />
   )
 }
 
